16b6a6e37165901b1ab217af19763b66ae01ebd2
LuisM11 <lumarinb@unal.edu.co>
2023-03-24 23:57:22 -0500

    login form validation with react hook form error UI  messages

--- src/modules/auth/pages/LoginPage.jsx
+++ src/modules/auth/pages/LoginPage.jsx
@@ -2,11 +2,18 @@ import React, { useRef, useState } from 'react'
 import bgVideo from '../../../assets/video.mp4'
 import Quizystem from '../../../assets/QLogo1.png'
 import DinaraLogo from '../../../assets/DLogo.png'
+import { useForm } from "react-hook-form";
 
 export const LoginPage = () => {
 
+  const {register,handleSubmit,formState: { errors }} = useForm();
+
   const email = useRef(null)
   const passwordRef = useRef(null)
+
+  const onSubmit = (data) => {
+    console.log(data,errors)
+  };
 
   return (
     <>
@@ -20,35 +27,49 @@ export const LoginPage = () => {
 
           <div>
             <figure>
-              <img src={Quizystem} alt="QuizystemLogo" />
+              <img onClick={()=> console.log(errors)} src={Quizystem} alt="QuizystemLogo" />
             </figure>
-            <form className='grid grid-cols-1 gap-1 w-full' action="">
-              <div className=''>
-                <label
-                  ref={email}
-                  htmlFor="email"
-                  className='auth-label-placeholder text-lg bg-white relative px-2 left-2'
-                >
-                  Email
-                </label>
-                <input
-                  onFocus={() => {
-                    email.current.classList.remove('auth-label-placeholder')
-                    email.current.classList.add('auth-label-transition')
-                  }}
-                  onBlur={(e) => {
-                    if (e.currentTarget.value === '') {
-                      email.current.classList.remove('auth-label-transition')
-                      email.current.classList.add('auth-label-placeholder')
-                    }
-                  }}
 
+            <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-1 gap-1 w-full' action="">
+              <div className=''>
+                <label ref={email} htmlFor="email" className='auth-label-placeholder text-lg bg-white relative px-2 left-2'> 
+                Email 
+                </label>
+                {email.current?
+                 errors.email ? email.current.classList.add('error-auth-label') : email.current.classList.remove('error-auth-label')
+                : undefined
+                }
+                <input
                   type="text"
                   name="email"
                   id="email"
-                  className='w-full px-3 border-[1px] h-12 rounded-md border-zinc-300 focus:border-primary focus:outline-none'
+                  {...register("email", {
+                    required: true,
+                    pattern: {
+                      value:
+                        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                      message:
+                        "Invalid email, please type your email again ",
+                    },
+                  })}
+                  onFocus={() => {
+                    email.current.classList.remove('auth-label-placeholder')
+                    email.current?.classList.add('auth-label-transition')              
+                  }}
+                  onBlur={(e) => {
+                    if (e.currentTarget.value === '') {
+                      email.current?.classList.remove('auth-label-transition')
+                      email.current?.classList.add('auth-label-placeholder')
+                    }
+                  }}
+                  className={ errors.email? 
+                    'w-full px-3 border-[1px] h-12 rounded-md border-red-600 focus:outline-none focus:border-red-600':
+                    'w-full px-3 border-[1px] h-12 rounded-md border-zinc-300 focus:border-primary focus:outline-none'}
                 />
+                {errors?.email?.type === 'required' ? <div className='pl-1 text-red-600 text-sm font-light'> Required </div> : undefined}
+                {errors?.email?.type === 'pattern' ? <div className='text-red-600 text-sm font-light'> {errors.email.message} </div> : undefined}
               </div>
+
 
               <div className='flex flex-col'>
                 <label
@@ -58,7 +79,19 @@ export const LoginPage = () => {
                 >
                   Password
                 </label>
+                {passwordRef.current?
+                 errors.password ? passwordRef.current.classList.add('error-auth-label') : passwordRef.current.classList.remove('error-auth-label')
+                : undefined
+                }
                 <input
+                  type="text"
+                  name="password"
+                  id="password"
+                  {...register("password", {
+                    required: true,
+                    maxLength: 25,
+                    minLength: 6,
+                  })}
                   onFocus={() => {
                     passwordRef.current.classList.remove('auth-label-placeholder')
                     passwordRef.current.classList.add('auth-label-transition')
@@ -68,12 +101,13 @@ export const LoginPage = () => {
                       passwordRef.current.classList.remove('auth-label-transition')
                       passwordRef.current.classList.add('auth-label-placeholder')
                     }
-                  }}
-                  type="text"
-                  name="password"
-                  id="password"
-                  className='w-full px-3 border-[1px] h-12 rounded-md border-zinc-300 focus:border-primary focus:outline-none'
+                  }}                  
+                  className={ errors.password? 
+                    'w-full px-3 border-[1px] h-12 rounded-md border-red-600 focus:outline-none focus:border-red-600':
+                    'w-full px-3 border-[1px] h-12 rounded-md border-zinc-300 focus:border-primary focus:outline-none'}
                 />
+                {errors?.password?.type === 'required' ? <div className='pl-1 text-red-600 text-sm font-light'> Required </div> : undefined}
+                {errors?.password?.type.includes("Length") ? <div className='text-red-600 text-sm font-light'> Min 6 - Max 25 </div> : undefined}
                 <span className='self-end text-sm text-primary cursor-pointer'>¿Forgot your password?</span>
               </div>
 
